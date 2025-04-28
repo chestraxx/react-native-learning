edbf4ccbe74791ab43e1e9a7a7ec6500a953b2db
Thai Duy <chestraxx@gmail.com>
2025-04-28 15:46:29 +0700

    feat: allow to add goal to goal list

--- 00RNProject/App.js
+++ 00RNProject/App.js
@@ -1,15 +1,36 @@
+import {useState} from 'react';
 import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setEnteredGoalText('');
+    setCourseGoals([...courseGoals, enteredGoalText]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
 
       <View style={styles.goalContainer}>
-        <Text style>List of Goals...</Text>
+        {courseGoals.map((goal, index) => (
+          <Text key={index} style={styles.goalItem}>
+            {goal}
+          </Text>
+        ))}
       </View>
     </View>
   );
@@ -49,5 +70,6 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
+    color: 'white',
   },
 });
