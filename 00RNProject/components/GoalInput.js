e3dd8bb7812d0ff11bd7e879c5e41dc7f4e7ca6d
Thai Duy <chestraxx@gmail.com>
2025-04-28 17:45:10 +0700

    feat: move goal input to modal

--- 00RNProject/components/GoalInput.js
+++ 00RNProject/components/GoalInput.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {StyleSheet, View, TextInput, Button} from 'react-native';
+import {StyleSheet, View, TextInput, Button, Modal} from 'react-native';
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -14,15 +14,21 @@ function GoalInput(props) {
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Your course goal!"
-        onChangeText={goalInputHandler}
-        value={enteredGoalText}
-      />
-      <Button title="Add Goal" onPress={addGoalHandler} />
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+
+        <View style={styles.buttonContainer}>
+          <Button title="Add Goal" onPress={addGoalHandler} />
+          <Button title="Cancel" color="red" onPress={props.onCancel} />
+        </View>
+      </View>
+    </Modal>
   );
 }
 
@@ -31,8 +37,8 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
@@ -46,6 +52,11 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   buttonContainer: {
-    width: '30%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '60%',
+    marginTop: 16,
+    padding: 16,
   },
 });
