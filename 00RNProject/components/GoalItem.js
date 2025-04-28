c1bf2441a602721da0653517951044bd87d1e40f
Thai Duy <chestraxx@gmail.com>
2025-04-28 17:27:59 +0700

    feat: add delete goal item on press

--- 00RNProject/components/GoalItem.js
+++ 00RNProject/components/GoalItem.js
@@ -1,10 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Pressable} from 'react-native';
 
 function GoalItem(props) {
   return (
-    <View style={styles.goalItem}>
-      <Text style={styles.goalItemText}>{props.text}</Text>
-    </View>
+    <Pressable onPress={props.onDeleteGoal.bind(this, props.id)}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalItemText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 }
 
